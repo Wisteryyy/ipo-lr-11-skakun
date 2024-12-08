793877f2b0f25cf9bdc32f62050f6f9a225656fc
Sofia <sofiaskakun30@gmail.com>
2024-12-08 21:19:20 +0300

    Vehicle.py

--- transport/Vehicle.py
+++ transport/Vehicle.py
@@ -4,7 +4,7 @@ class Vehicle: # определяем класс Vehicle
     def __init__(self, capacity): # конструктор класса
         if not isinstance(capacity, (int, float)) or capacity < 0: # проверка, является ли capacity положительным числом
             raise ValueError("Грузоподьемность должна быть положительным числом.")
-
+        
         self.vehicle_id = str(uuid.uuid4()) # генерируем уникальный идентификатор для транспортного средства
         self.capacity = capacity
         self.current_load = 0
@@ -17,7 +17,7 @@ class Vehicle: # определяем класс Vehicle
         cargo_weight = client.cargo_weight # достаем вес груза из объекта client
         if not isinstance(cargo_weight, (float, int)) or cargo_weight < 0: # проверка, является ли cargo_weight положительным числом
             raise ValueError("Вес груза должен быть положительным числом.")
-
+        
         if self.current_load + cargo_weight > self.capacity: # проверка, не превышает ли текущая загрузка плюс вес груза грузоподъемность
             raise ValueError("Недостаточно грузоподъемности для загрузки данного груза.")
         
