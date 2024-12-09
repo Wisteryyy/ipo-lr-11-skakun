72d2fcf93fbfda6528110974ad5c4df2e00bcc79
Sofia <sofiaskakun30@gmail.com>
2024-12-09 14:29:38 +0300

    Truck.py

--- transport/Truck.py
+++ transport/Truck.py
@@ -1,11 +1,14 @@
 from .Vehicle import Vehicle
 
 class Truck(Vehicle):
+    _id_counter = 1  # Счетчик для уникальных ID
     def __init__(self, capacity, color):
         super().__init__(capacity)
         if not isinstance(color, str) or not color.strip(): # проверка на всякий бред по типу пробела вместо строки
             raise ValueError("Цвет должен быть непустой строкой.")
         
+        self.id = Truck._id_counter
+        Truck._id_counter += 1
         self.color = color  # цвет грузовика
 
     def __str__(self):
